8ff5f172e2fad6f9bc3ccf5120dc77a764b3a439
Jxrgee8 <dejorge.mh@gmail.com>
2024-03-04 17:37:47 +0100

    Manager Método count(generos): - Se crea un método en SerieRepository para obtener los generos y el número de series con estos - Se crea un manager template de prueba pàra ver la visualización - TODO: cambiar los datos del gráficos usando los datos de la BBDD (ManagerController.php)

--- src/Controller/Manager/ManagerController.php
+++ src/Controller/Manager/ManagerController.php
@@ -11,6 +11,7 @@
 
 namespace App\Controller\Manager;
 
+use App\Repository\SerieRepository;
 use App\Service\UxPackageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,19 +22,24 @@ use Symfony\UX\Chartjs\Model\Chart;
 class ManagerController extends AbstractController
 {
     #[Route('/manager', name: 'manager')]
-    public function __invoke(UxPackageRepository $packageRepository, ChartBuilderInterface $chartBuilder): Response
+    public function __invoke(UxPackageRepository $packageRepository, ChartBuilderInterface $chartBuilder, SerieRepository $serieRepository): Response
     {
         $package = $packageRepository->find('chartjs');
-        $labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-        // TODO: obtener data de la base de datos:
-        $data = [12, 20, 32, 45, 62, 50, 15, 26, 40, 38, 22, 65, 70];
+
+        // TODO: Obtener datos de la BBDD:
+        $array_generos = $serieRepository->countGeneros();
+
+        // ver: https://dev.to/mis0u/symfony-ux-and-chartjs-ff4
+        // ver: https://dev.to/qferrer/creating-a-covid-19-data-visualization-with-symfony-ux-chart-js-2khj#render-chart
+        $labels = [];
+        $data = [];
 
         $chart = $chartBuilder->createChart(Chart::TYPE_BAR);
         $chart->setData([
             'labels' => $labels,
             'datasets' => [
                 [
-                    'label' => 'Series Vistas',
+                    'label' => 'Géneros Más Comunes',
                     'barPercentage' => 0.5,
                     'barThickness' => 40,
                     'data' => $data,
@@ -54,6 +60,15 @@ class ManagerController extends AbstractController
         return $this->render('manager/manager.html.twig', [
             'package' => $package,
             'chart' => $chart,
+        ]);
+    }
+
+    #[Route('/manager_prueba', name: 'manager_prueba')]
+    public function prueba(SerieRepository $serieRepository): Response {
+        $array_generos = $serieRepository->countGeneros();
+
+        return $this->render('manager/manager_prueba.html.twig', [
+            'array_generos' => $array_generos 
         ]);
     }
 }
--- src/Repository/SerieRepository.php
+++ src/Repository/SerieRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Serie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -19,5 +20,19 @@ class SerieRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Serie::class);
+    }
+
+    public function countGeneros(): ?array {
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "
+            SELECT G.nombre, COUNT(GS.genero_id) AS num_generos FROM genero G 
+            INNER JOIN genero_serie GS ON G.id = GS.genero_id 
+            INNER JOIN serie S ON S.id = GS.serie_id 
+            GROUP BY GS.genero_id
+            ";
+
+        $resultSet = $conn->executeQuery($sql);
+
+        return $resultSet->fetchAllAssociative();
     }
 }
