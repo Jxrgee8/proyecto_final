b304853279ea8674e3a4098d00a23e8120cd0740
Jxrgee8 <dejorge.mh@gmail.com>
2024-03-08 18:08:46 +0100

    Series Home Dinámico: - Las series mostradas en Home (viendo y por_ver) aparecen ahora de forma dinámica dependiendo del usuario - El apartado de series por ver mostrará un máximo de 6 series

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\ListaRepository;
+use App\Repository\SerieListaRepository;
 use App\Repository\SerieRepository;
+use App\Repository\UsuarioRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +13,48 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(SerieRepository $serieRepository): Response
+    public function index(UsuarioRepository $usuarioRepository, ListaRepository $listaRepository, SerieRepository $serieRepository, SerieListaRepository $serieListaRepository): Response
     {
+        // Recoger Usuario actual:
+        $user = $this->getUser();
+        $currentUser = $usuarioRepository->getUserID($user->getUserIdentifier());
+        $currentUserID = $currentUser->getId();
 
-        $array_series = $serieRepository->findAll();
+        // Obtener Listas viendo y por ver
+        $listaUsuarioViendo = $listaRepository->listaSeriesViendo($currentUserID);
+        $listaUsuarioPorVer = $listaRepository->listaSeriesPorVer($currentUserID);
+
+        // Obtener ID de las listas:
+        $currentListaViendoID = $listaUsuarioViendo->getId();
+        $currentListaPorVerID = $listaUsuarioPorVer->getId(); 
+
+        // Obtener un array con todas las series de la lista con los ID obtenidos antes (SerieLista):
+        $series_lista_viendo = $serieListaRepository->getSerieIdFromLista($currentListaViendoID);
+        $series_lista_por_ver = $serieListaRepository->getSerieIdFromLista($currentListaPorVerID);
+
+        // Se guardan los ids de las series obtenidas en un array seleccionando esa clave-valor (array['serie_id']):
+        $array_id_series_viendo = [];
+        foreach ($series_lista_viendo as $id_serie) {
+            $array_id_series_viendo[] = $id_serie['serie_id'];
+        }
+
+        $array_id_series_por_ver = [];
+        foreach ($series_lista_por_ver as $id_serie) {
+            $array_id_series_por_ver[] = $id_serie['serie_id'];
+        }
+
+        // Se busca en "serieRepository" las series con los ids obtenidos antes y se guardan todas las series en un array de objetos (Serie[id, nombre, ...]):
+        foreach ($array_id_series_viendo as $id_serie) {
+            $array_series_viendo[] = $serieRepository->find($id_serie);
+        }  
+        
+        foreach ($array_id_series_por_ver as $id_serie) {
+            $array_series_por_ver[] = $serieRepository->find($id_serie);
+        }    
 
         return $this->render('page/home/home.html.twig', [
-            'array_series' => $array_series,
-            'controller_name' => 'HomeController',
+            'array_series_viendo' => $array_series_viendo,
+            'array_series_por_ver' => $array_series_por_ver,
         ]);
     }
 }
